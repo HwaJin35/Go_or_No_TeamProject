70083760aa7496562013f7f02a2bd6143d688160
HWAJIN <jonghwajin@gmail.com>
2025-04-03 15:32:27 +0900

    스프링부트 베이스 패키지 커밋

---
+++ canigo/src/test/java/com/goorno/canigo/CanigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorno.canigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
