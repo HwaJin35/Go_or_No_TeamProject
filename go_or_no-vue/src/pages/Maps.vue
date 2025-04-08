d0374c066da82311e6ed84b212544624402be4b8
HWAJIN <jonghwajin@gmail.com>
2025-04-08 15:29:16 +0900

    카카오 맵 등록 성공 커밋

--- go_or_no-vue/src/pages/Maps.vue
+++ go_or_no-vue/src/pages/Maps.vue
@@ -1,87 +1,38 @@
 <template>
-  <card class="card-map" title="Google Maps">
+  <card class="card-map" title="카카오 맵">
     <div class="map">
       <div id="map"></div>
     </div>
   </card>
 </template>
 <script>
-export default {
-  mounted() {
-    var myLatlng = new window.google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-      zoom: 13,
-      center: myLatlng,
-      scrollwheel: false, // we disable de scroll over the map, it is a really annoing when you scroll through page
-      styles: [
-        {
-          featureType: "water",
-          stylers: [{ saturation: 43 }, { lightness: -11 }, { hue: "#0088ff" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.fill",
-          stylers: [
-            { hue: "#ff0000" },
-            { saturation: -100 },
-            { lightness: 99 },
-          ],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#808080" }, { lightness: 54 }],
-        },
-        {
-          featureType: "landscape.man_made",
-          elementType: "geometry.fill",
-          stylers: [{ color: "#ece2d9" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "geometry.fill",
-          stylers: [{ color: "#ccdca1" }],
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#767676" }],
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.stroke",
-          stylers: [{ color: "#ffffff" }],
-        },
-        { featureType: "poi", stylers: [{ visibility: "off" }] },
-        {
-          featureType: "landscape.natural",
-          elementType: "geometry.fill",
-          stylers: [{ visibility: "on" }, { color: "#b8cb93" }],
-        },
-        { featureType: "poi.park", stylers: [{ visibility: "on" }] },
-        {
-          featureType: "poi.sports_complex",
-          stylers: [{ visibility: "on" }],
-        },
-        { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
-        {
-          featureType: "poi.business",
-          stylers: [{ visibility: "simplified" }],
-        },
-      ],
-    };
-    var map = new window.google.maps.Map(
-      document.getElementById("map"),
-      mapOptions
-    );
+  export default{
+    mounted(){
+      if(window.kakao && window.kakao.maps){
+        this.initMap();
+      } else {
+        // 로딩이 느릴 경우를 대비 ready 콜백
+        window.kakao.maps.load(this.initMap);
+      }
+    },
+    methods: {
+      initMap(){
+        const container = document.getElementById("map");
+        const options = {
+          center: new window.kakao.maps.LatLng(37.5665, 126.9780),
+        level: 3,
+      };
 
-    var marker = new window.google.maps.Marker({
-      position: myLatlng,
-      title: "Hello World!",
-    });
+      const map = new window.kakao.maps.Map(container, options);
 
-    // To add the marker to the map, call setMap();
-    marker.setMap(map);
+      //마커 추가 예시
+      const markerPosition = new window.kakao.maps.LatLng(37.5665, 126.9780);
+      const marker = new window.kakao.maps.Marker({
+        position: markerPosition,
+      });
+
+      marker.setMap(map);
+    },
   },
 };
 </script>
