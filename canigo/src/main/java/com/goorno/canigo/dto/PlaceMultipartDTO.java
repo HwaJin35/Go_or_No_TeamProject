c0e0466418698b9d637e62b382442653d313fcaf
EODOHA <eoryk96@naver.com>
2025-04-10 14:15:11 +0900

    주석 추가

--- canigo/src/main/java/com/goorno/canigo/dto/PlaceMultipartDTO.java
+++ canigo/src/main/java/com/goorno/canigo/dto/PlaceMultipartDTO.java
@@ -6,7 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PlaceMultipartDTO extends CommonMultipartDTO {
-	private String name;
-	private Double latitude;
-	private Double longitude;
+	private String name; // 장소 이름
+	private Double latitude; // 위도
+	private Double longitude; // 경도
 }
+
+// 장소 등록에 필요한 정보를 담는 DTO
+// PlaceController에서 @ModelAttribute로 이 클래스를 받음
